76cc5483c759bfb4cbd0fa3e0b9f696215c3c6ba
Rishu Roy <rishukumar.prince@gmail.com>
2020-09-17 21:19:30 +0530

    SqlFlight Pligin Added And Helper File Added

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/place.dart';
 import 'dart:io';
+import '../helpers/db_helper.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -18,5 +19,10 @@ class GreatPlaces with ChangeNotifier {
     );
     _items.add(newPlace);
     notifyListeners();
+    DBHelper.insert('places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
   }
 }

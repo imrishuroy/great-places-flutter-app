130e9e34c504a279d663527eca04104e8c093e8a
Rishu Roy <rishukumar.prince@gmail.com>
2020-09-18 22:13:47 +0530

    Fetching User Coordinates

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/location_input.dart';
 import '../providers/great_places.dart';
 import 'package:provider/provider.dart';
 import '../widgets/image_input.dart';
@@ -52,6 +53,10 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                       height: 10,
                     ),
                     ImageInput(_selectImage),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    LocationInput(),
                   ],
                 ),
               ),
